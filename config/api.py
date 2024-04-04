875cf6d0b2cd93192e010cfecf648f210aa3b766
Michael Stewart <stewarmc@jmu.edu>
2024-04-04 10:58:50 -0400

    try this!

--- config/api.py
+++ config/api.py
@@ -7,14 +7,14 @@ from allauth.socialaccount.models import SocialAccount
 api = NinjaAPI()
 
 
-# @api.get("/enrollments")
-# def enrollments(request):
-#     sa = SocialAccount.objects.filter(user=self.request.user).first()
-#     token = sa.socialtoken_set.first()
-#     if token is not None:
-#         canvas = Canvas("https://canvas.jmu.edu", token.token)
-#         courses = canvas.get_courses()
-#         return courses
+@api.get("/enrollments")
+def enrollments(request):
+    sa = SocialAccount.objects.filter(user=request.user).first()
+    token = sa.socialtoken_set.first()
+    if token is not None:
+        canvas = Canvas("https://canvas.jmu.edu", token.token)
+        courses = [c for c in canvas.get_courses()]
+        return len(courses)
 #     canvas = Canvas("https://canvas.jmu.edu", request)
 #     # r = requests.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))
 #     return "Hello world"
